c7bdb866e12d19b5fc89a85f5a4e074713582ebf
Vali M <marginean.vali@gmail.com>
2021-03-12 18:30:28 +0200

    Batching in case of large periods. Day of Week method

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ function getCookieValue(key){
       .split('=')[1]
 };
 
-async function getSevenPostTimestampsAfterIndex(idx) {
-    const url = "https://www.xing.com/communities/forums/100004482/endless.json?offset=" + idx;
-    const timestamps = await fetch(url, {
+async function getSevenPostTimestampsAfterIndex(idx, url) {
+    if(!url.startsWith("https://www.xing.com/communities/forums")){
+        console.error("Current Page needs to start with 'https://www.xing.com/communities/forums'!");
+        alert("Current Page needs to start with 'https://www.xing.com/communities/forums'!");
+        return;
+    }
+    const fullUrl = url + "/endless.json?offset=" + idx;
+    const timestamps = await fetch(fullUrl, {
       "headers": {
         "accept": "text/javascript, text/html, application/xml, text/xml, */*",
         "accept-language": "en-US,en;q=0.9,en-GB;q=0.8,de;q=0.7",
@@ -34,16 +39,26 @@ async function getSevenPostTimestampsAfterIndex(idx) {
     return timestamps.map(str => str.substring(11,str.length - 2));
 };
 
-async function getLastPostTimestamps(numberOfPosts){
-    const fetches = [];
-    for(let i = 0; i < numberOfPosts + 7; i += 7){
-        fetches.push(getSevenPostTimestampsAfterIndex(i));
+async function _getLastPostTimestamps(numberOfPosts, url){
+    if(typeof _=="undefined"){
+        console.error("Need to import Lodash first!");
+        alert("Need to import Lodash first!");
+        return;
     }
-    const promiseResults = await Promise.all(fetches);
-    const flatTimestampArr = promiseResults.flat(1);
-    return flatTimestampArr;
-    return await mockData;
-};
+    const fetches = [];
+    const batches = numberOfPosts / 1200;
+    let flatResults = [];
+    for(let batchIdx = 0; batchIdx < batches; batchIdx++){
+        for(let i = batchIdx * 1200; i < (batchIdx + 1) * 1200; i += 7){
+            fetches.push(getSevenPostTimestampsAfterIndex(i, url));
+        }
+        const promiseResults = await Promise.all(fetches);
+        flatResults = flatResults.concat(promiseResults.flat(1));
+    }
+    return flatResults;
+}
+
+let getLastPostTimestamps = _.memoize(_getLastPostTimestamps);
 
 async function displayLastPostDates(numberOfPosts) {
     const dateFormatFn = (date) => date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
@@ -60,6 +75,11 @@ async function displayLastPostTimeOnly(numberOfPosts) {
     return await displayLastPostTimestampsWithFormat(numberOfPosts, dateFormatFn);
 }
 
+async function displayLastPostDayOfWeek(numberOfPosts) {
+    const dateFormatFn = (date) =>  date.getDay();
+    return await displayLastPostTimestampsWithFormat(numberOfPosts, dateFormatFn);
+}
+
 function countOcurrencesReductionFn (acc, curr) {
      if (typeof acc[curr] == 'undefined') {
        acc[curr] = 1;
@@ -71,9 +91,10 @@ function countOcurrencesReductionFn (acc, curr) {
 }
 
 async function displayLastPostTimestampsWithFormat(numberOfPosts, dateFormatFn) {
-    const timestamps = await getLastPostTimestamps(numberOfPosts);
+    const timestamps = await getLastPostTimestamps(numberOfPosts, window.location.href);
     const dates = timestamps
             .map((timestamp) => new Date(timestamp))
+            //TODO: Convert to german time?
             .map(dateFormatFn);
     const dateToCountDict = dates.reduce(countOcurrencesReductionFn, {});
     let result = '';
